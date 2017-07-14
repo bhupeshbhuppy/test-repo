a41af48ccc00ca54b2007af66bc56433ea9deb05
Bhupesh Joshi <bhupesh.bhuppy@gmail.com>
2017-07-14 20:53:56 +0530

    files

--- Assignment1_V1.0.r
+++ Assignment1_V1.0.r
@@ -4,6 +4,9 @@ library(plotly)
 library(reshape2)
 library(corrplot)
 library(binr)
+library(xgboost)
+library(Metrics)
+library(performanceEstimation)
 sharing_data<-read.csv("D:/bhupesh/Bootcamp/BootCamp Assignment/OnlineNewsPopularity/OnlineNewsPopularity.csv")
 
 ##Looking at the data file##
@@ -14,8 +17,8 @@ summary(sharing_data)
 
 dummy_Features<-c("data_channel_is_lifestyle","data_channel_is_bus",
                   "data_channel_is_socmed","data_channel_is_tech",
-                  "data_channel_is_world","weekday_is_monday",
-                  "data_channel_is_entertainment",
+                  "data_channel_is_world",
+                  "data_channel_is_entertainment","weekday_is_monday",
                   "weekday_is_tuesday","weekday_is_wednesday",
                   "weekday_is_thursday","weekday_is_friday",
                   "weekday_is_saturday","weekday_is_sunday","is_weekend")
@@ -23,6 +26,7 @@ conti_features<-colnames(sharing_data)[!(colnames(sharing_data) %in% dummy_Featu
 conti_data<-sharing_data[,conti_features]
 conti_data<-conti_data[,-c(1:2)]
 dummy_data<-sharing_data[,dummy_Features]
+dummy_data$shares<-sharing_data$shares
 
 
 table(sharing_data[,"data_channel_is_lifestyle"])
@@ -159,6 +163,7 @@ temp_melt<-melt(conti_data[6:11])
 p<-ggplot(temp_melt,aes(value, fill = variable ))+geom_density(alpha = 0.5)+ggtitle("Density Plots")
 ggplotly(p)%>%
   layout(plot_bgcolor="transparent",paper_bgcolor= "transparent")
+
 #### Most of the distribution for Digital Media is skewed therefore can take log
 
 temp_melt<-melt(conti_data[12:20])
@@ -222,3 +227,135 @@ corrplot(cor(conti_data[38:44]), method = "circle")
 #### There is some degree of correlation amongst variable therefore we can go for Group wise PCA
 
 
+### Discrete Variable
+
+channel_features<-dummy_data[,c(1:6)]
+k<-c()
+for(i in colnames(channel_features)){
+temp_var<-data.frame(table(channel_features[,i]))
+temp_var$Var2<-i
+k<-rbind(k,temp_var)
+}
+plot_ly(k, x = ~Var2, y =~Freq , color = ~Var1)%>%
+  layout(title ="Missing vs Response Chart")
+
+############ MODEL1- Data set 1 ( Feature engineering)###############
+
+sharing_data_f1<-sharing_data
+
+###  kw_min_min -1 as o
+sharing_data_f1$kw_min_min[sharing_data_f1$kw_min_min == -1]<-0
+summary(sharing_data_f1$kw_min_min)
+
+### Binning of data
+## n_token_title 13 bins 
+cuts<-bins(sharing_data_f1$n_tokens_title,13,minpts =  1, exact.groups = FALSE)
+sharing_data_f1$n_tokens_title_f<-as.integer(cut(sharing_data_f1$n_tokens_title, bins.getvals(cuts)))
+## average_token_length 2 bins
+cuts<-bins(sharing_data_f1$average_token_length,2,minpts =  1, exact.groups = FALSE)
+sharing_data_f1$average_token_length_f<-as.integer(cut(sharing_data_f1$average_token_length, bins.getvals(cuts)))
+
+## num_videos 5 bins
+cuts<-bins(sharing_data_f1$num_videos,5,minpts =  1, exact.groups = FALSE)
+sharing_data_f1$num_videos_f<-as.integer(cut(sharing_data_f1$num_videos, bins.getvals(cuts)))
+## num_keywords 8 bin (generated 7)
+cuts<-bins(sharing_data_f1$num_keywords,8,minpts =  1, exact.groups = FALSE)
+sharing_data_f1$num_keywords_f<-as.integer(cut(sharing_data_f1$num_keywords, bins.getvals(cuts)))
+## kw_min_min 3 bins (generated 2)
+cuts<-bins(sharing_data_f1$kw_min_min,3,minpts =  1, exact.groups = FALSE)
+sharing_data_f1$kw_min_min_f<-as.integer(cut(sharing_data_f1$kw_min_min, bins.getvals(cuts)))
+## kw_max_max 4 bins
+cuts<-bins(sharing_data_f1$kw_max_max,4,minpts =  1, exact.groups = FALSE)
+sharing_data_f1$kw_max_max_f<-as.integer(cut(sharing_data_f1$kw_max_max, bins.getvals(cuts)))
+summary(sharing_data_f1[,c(62:67)])
+
+
+## randomly divide data 
+sharing_data_f1<-sharing_data_f1[,-c(1,2)]
+sharing_data_f1$random <- runif(nrow(sharing_data_f1))
+train <- sharing_data_f1[sharing_data_f1$random <= 0.7,] 
+test <- sharing_data_f1[sharing_data_f1$random > 0.7,] 
+col_name<-colnames(train[,-c(59,66)])
+col_name_base<-col_name[1:56]
+## xgboost
+model_xg_base<-xgboost(data        = data.matrix(train[,col_name_base]),
+                  label       = train$shares,
+                  nrounds     = 100,
+                  objective   = "reg:linear",
+                  eval_metric = "rmse")
+test$Prediction_base<-as.integer(round(predict(model_xg, data.matrix(test[,col_name_base]))))
+rmse(test$shares,test$Prediction_base)
+model_xg<-xgboost(data        = data.matrix(train[,col_name]),
+                 label       = train$shares,
+                 nrounds     = 500,
+                 objective   = "reg:linear",
+                 eval_metric = "rmse")
+test$Prediction<-as.integer(round(predict(model_xg, data.matrix(test[,col_name]))))
+imp_matrix<-xgb.importance(colnames(train[,col_name]),model_xg)
+xgb.plot.importance(imp_matrix)
+rmse(test$shares,test$Prediction)
+
+
+######## other performance metrices
+
+cuts<-bins(sharing_data_f1$shares,5,minpts =  1, exact.groups = FALSE)
+cuts$binct
+test$actual_bins<-as.integer(cut(test$shares, bins.getvals(cuts)))
+test$predicted_bins<-as.integer(cut(test$Prediction, bins.getvals(cuts)))
+kappa_xg<-ScoreQuadraticWeightedKappa(test$predicted_bins,test$actual_bins)
+
+####dividing the data itself
+cuts<-bins(sharing_data_f1$shares,2,minpts =  1, exact.groups = FALSE)
+train$shares_bin_2<-as.integer(cut(train$shares, bins.getvals(cuts)))
+test$shares_bin_2<-as.integer(cut(test$shares, bins.getvals(cuts)))
+model_xg<-xgboost(data        = data.matrix(train[,col_name]),
+                  label       = train$shares,
+                  nrounds     = 500,
+                  objective   = "reg:linear",
+                  eval_metric = "rmse")
+table(train$shares_bin_2)
+test$shares_bin_2<-test$shares_bin_2-1
+train$shares_bin_2<-train$shares_bin_2-1
+### 0  is unpopular
+model_1_data<-train[which(train$shares_bin_2 == 0),]
+model_2_data<-train[which(train$shares_bin_2 == 1),]
+### unpopular
+model_1_xg<-xgboost(data        = data.matrix(model_1_data[,col_name]),
+                  label       = model_1_data$shares,
+                  nrounds     = 500,
+                  objective   = "reg:linear",
+                  eval_metric = "rmse")
+### popular
+model_2_xg<-xgboost(data        = data.matrix(model_2_data[,col_name]),
+                              label       = model_2_data$shares,
+                              nrounds     = 500,
+                              objective   = "reg:linear",
+                              eval_metric = "rmse")
+### binning
+model_3_xg<-xgboost(data        = data.matrix(train[,col_name]),
+                    label       = train$shares_bin_2,
+                    nrounds     = 500,
+                    objective   = "binary:logistic",
+                    eval_metric = "rmse")
+test$Predict_share_bin_2<-as.numeric(test$Predict_share_bin_2 > 0.5)
+table(test$Predict_share_bin_2)
+classificationMetrics(test$shares_bin_2,test$Predict_share_bin_2)
+
+Predict_share_0<-as.integer(predict(model_1_xg,data.matrix(test[which(test$Predict_share_bin_2 == 0),col_name])))
+rmse(test$shares[test$Predict_share_bin_2 == 0],Predict_share_0)
+Predict_share_1<-as.integer(predict(model_2_xg,data.matrix(test[which(test$Predict_share_bin_2 == 1),col_name])))
+rmse(test$shares[test$Predict_share_bin_2 == 1],Predict_share_1)
+index_0<-1
+index_1<-1
+for(i in 1:length(test[,1])){
+  if(test$shares_bin_2[i]== 0){
+    test$Prediction[i]<-Predict_share_0[index_0]
+    index_0<-index_0 + 1
+  }
+  else{
+    test$Prediction[i]<-Predict_share_1[index_1]
+    index_1<-index_1 + 1
+  }
+}
+rmse(test$shares, test$Prediction)
+summary(test$Prediction)
